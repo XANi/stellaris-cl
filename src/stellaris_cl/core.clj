65652908a030522bdb25e53f1ab50e1cdb2dffec
Mariusz Gronczewski <xani666@gmail.com>
2018-11-08 02:10:01 +0100

    it fucking parses

--- src/stellaris_cl/core.clj
+++ src/stellaris_cl/core.clj
@@ -6,16 +6,13 @@
   )
 
 
-
-
-
 ;(def grammar (clojure.java.io/resource "grammar/stellaris-commentless.bnf"))
 (def grammar (clojure.java.io/resource "grammar/stellaris.bnf"))
 (def jsgrammar (clojure.java.io/resource "grammar/json.bnf"))
 
 (def example-tiny (slurp(clojure.java.io/resource "t/example-tiny")))
-;(def example-1  (slurp(clojure.java.io/resource "t/00_buildings.txt")))
-(def example-1  (slurp(clojure.java.io/resource "t/example2")))
+(def example-1  (slurp(clojure.java.io/resource "t/00_buildings.txt")))
+;(def example-1  (slurp(clojure.java.io/resource "t/example2")))
 ;(def example-1  (slurp(clojure.java.io/resource "t/00_event_buildings.txt")))
 (defn strip-comment [x] (apply str (take-while #(not (#{\# \;} %)) x)))
 (def stellar-parse
@@ -25,5 +22,18 @@
 (print "\n-----\n")
 ;(pprint (stellar-parse example-file :total true))
 (def data (string/join "\n" (map strip-comment (clojure.string/split-lines example-1))))
-(print (stellar-parse data :unhide :all))
-(time(pprint(stellar-parse data)))
+;(print (stellar-parse data :unhide :all :total true))
+;(print (stellar-parse data :unhide :all ))
+;(pprint (stellar-parse data ))
+(print "\n-----\n")
+(pprint (stellar-parse data ))
+(print "\n-----\n")
+(time(stellar-parse data))
+(print "\n")
+(time(stellar-parse data))
+(print "\n")
+(time(stellar-parse data))
+(print "\n")
+(time(stellar-parse data))
+(print "\n")
+(print "\n-----\n")
